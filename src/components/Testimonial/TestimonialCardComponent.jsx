1b49da0beb5495443df429dd8faf8cf80c337f9e
Melvin Lilis <vhinlilis07@gmail.com>
2025-01-11 21:54:27 +0800

    Added react slick libraries for student tesimonial carousel

--- src/components/Testimonial/TestimonialCardComponent.jsx
+++ src/components/Testimonial/TestimonialCardComponent.jsx
@@ -1,22 +1,47 @@
+import Slider from "react-slick";
 import style from "../../css/testimonial.module.css";
 
 const TestimonialCardComponent = ({ students }) => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className={style["body-container"]}>
-      {students.map((student, index) => (
-        <div key={index} className={style["student-card"]}>
-          <div className={style["testimonial-box"]}>
-            <img src="src/assets/quote-icon.png" alt="Double-Quote Icon" />
-            <p>{student.testimonial}</p>
-            <img src="src/assets/star-rating.png" alt="Star Rating Icon" />
+      <Slider {...settings}>
+        {students.map((student, index) => (
+          <div key={index} className={style["student-card"]}>
+            <div className={style["testimonial-box"]}>
+              <img src="src/assets/quote-icon.png" alt="Double-Quote Icon" />
+              <p>{student.testimonial}</p>
+              <img src="src/assets/star-rating.png" alt="Star Rating Icon" />
+            </div>
+            <div className={style["student-box"]}>
+              <img src={student.img_url} alt={student.img_url} />
+              <h3>{student.name}</h3>
+              <h6>Gym Student</h6>
+            </div>
           </div>
-          <div className={style["student-box"]}>
-            <img src={student.img_url} alt={student.img_url} />
-            <h3>{student.name}</h3>
-            <h6>Gym Student</h6>
-          </div>
-        </div>
-      ))}
+        ))}
+      </Slider>
     </div>
   );
 };
