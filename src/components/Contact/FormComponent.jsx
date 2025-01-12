8e40c24815f3db244f18dc9e009e8a27454c96ab
Melvin Lilis <vhinlilis07@gmail.com>
2025-01-12 21:53:16 +0800

    Installed emailjs library and connect email form settings

--- src/components/Contact/FormComponent.jsx
+++ src/components/Contact/FormComponent.jsx
@@ -1,14 +1,35 @@
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 import style from "../../css/contact.module.css";
 
 const FormComponent = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_ypumvse", "template_h12se2k", form.current, {
+        publicKey: "wE6bJs9oU0nBZsIyj",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          e.target.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <div className={style["form-container"]}>
       <h3>{`let's connect`}</h3>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" name="" id="" placeholder="Email Address" />
-        <textarea name="" id="" placeholder="Message" rows="4"></textarea>
-        <button>send message</button>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="user_name" placeholder="Name" />
+        <input type="email" name="user_email" placeholder="Email Address" />
+        <textarea name="message" placeholder="Message" rows="4"></textarea>
+        <button type="submit">send message</button>
       </form>
     </div>
   );
